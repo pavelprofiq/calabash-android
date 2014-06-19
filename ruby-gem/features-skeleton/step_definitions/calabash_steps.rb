cdd72d3a397cc8725b12ce6025c706d46fc13444
pavelprofiq <pavel.juchelka@profiq.cz>
2014-06-19 14:56:13 +0200

    Update calabash_steps.rb

--- ruby-gem/features-skeleton/step_definitions/calabash_steps.rb
+++ ruby-gem/features-skeleton/step_definitions/calabash_steps.rb
@@ -9,6 +9,7 @@ end
 
 Then(/^I wait, until load Space$/) do
   wait_for_elements_exist(["* id:'action_bar_title'"], :timeout => 70)
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 120)
 end
 
 
@@ -41,13 +42,12 @@ Then(/^I press Log in button$/) do
 end
 
 Then(/^I press Next button$/) do
-  wait_for_elements_exist(["* id:'login_new_org_button'"], :timeout => 20)
+  wait_for_elements_exist(["* id:'login_new_org_button'"], :timeout => 40)
   touch "button id:'login_new_org_button'"
 end
 
 
 Then(/^I open organization list menu$/) do
-
   wait_for_elements_exist(["* id:'action_bar_title'"], :timeout => 60)
   sleep(0.1)
   touch "textview id:'action_bar_title'"
@@ -58,11 +58,13 @@ Then(/^I press Account button$/) do
 end
 
 Then(/^I add favorite$/) do
+  performAction('wait_for_no_progress_bars') 
   touch "imagebutton id:'favorite'"
 end
 
 
 Then(/^I press delete organization$/) do
+  wait_for_elements_exist(["* id:'delete_org'"], :timeout => 60)
   touch "button id:'delete_org'"
 end
 
@@ -77,10 +79,12 @@ end
 
 Then(/^I press on keyboard "(.*?)" as SAML email$/) do |arg1|
   system("adb shell input text treti@se.cz")
+  perform_action('keyboard_enter_text', arg1)
 end
 
 Then(/^I press on keyboard "(.*?)" as SAML password$/) do |arg1|
   system("adb shell input text sasasasa1")
+  perform_action('keyboard_enter_text', arg1)
 end
 
 Then(/^I press on keyboard "(.*?)" as manager org$/) do |arg1|
@@ -120,6 +124,7 @@ end
 
 Then(/^I open favorite "(.*?)"$/) do |text|
   sleep(1)
+  wait_for( timeout: 60  ) { query("textview", 'text') }
   touch "textview text:'#{text}'"
 end
 
@@ -166,6 +171,8 @@ Then(/^I press Sign in/) do
 end
 
 Then(/^I open tab view$/) do
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 5)
+  touch "* id:'action_show_toolbar'"
   wait_for_elements_exist(["* id:'tabs'"], :timeout => 120)
   touch "ImageButton id:'tabs'"
 end
@@ -211,8 +218,13 @@ Then(/^I open action toolbar button$/) do
   touch "* id:'action_show_toolbar'"
 end
 
-Then(/^I enter into manager organization name "(.*?)" email "(.*?)" password "(.*?)"$/) do |arg1, arg2, arg3|
+Then(/^I close action toolbar button$/) do
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 5)
+  touch "* id:'action_show_toolbar'"
+end
 
+Then(/^I enter into manager organization name "(.*?)" email "(.*?)" password "(.*?)"$/) do |arg1, arg2, arg3|
+  performAction('wait_for_no_progress_bars') 
   set_text "webView css:input[id=textfield-1017-inputEl]", arg2
   set_text "webView css:input[id=textfield-1021-inputEl]", arg3
   set_text "webView css:input[id=textfield-1016-inputEl]", arg1
@@ -226,11 +238,6 @@ Then(/^I enter "(.*?)" as the email$/) do |arg1|
   wait_for_elements_exist(["* id:'loginTextField'"], :timeout => 120)
   query "edittext id:'loginTextField'", :setText => arg1
 end
-
-Then(/^I try to find login element form from Act-On$/) do
-  set_text "webView css:input[id=login]",  :setText => 'idk'
-end
-
 
 
 Then(/^I slide down in list of apps$/) do
@@ -254,7 +261,7 @@ Then(/^I wait until I see entering organization name page$/) do
   performAction('wait_for_text', "Organization name", 120)
 end
 
-Then(/^I do a long press on a link$/) do
+Then(/^I do a long press on a link - Nexus 5$/) do
   performAction("long_press_coordinate", 300, 900)
 end
 
@@ -265,6 +272,31 @@ end
 
 Then(/^I login to Sace with org "(.*?)" email "(.*?)" password "(.*?)" and PINs "(.*?)"$/) do |arg1,arg2,arg3,arg4|
   wait_for_elements_exist(["* id:'org_name'"], :timeout => 20)
+  touch "* id:'org_name'"
+  query "edittext id:'org_name'", :setText => arg1
+  wait_for_elements_exist(["* id:'login_new_org_button'"], :timeout => 20)
+  touch "button id:'login_new_org_button'"
+  wait_for_elements_exist(["* id:'loginTextField'"], :timeout => 20)
+  touch "* id:'loginTextField'"
+  query "edittext id:'loginTextField'", :setText => arg2 
+  wait_for_elements_exist(["* id:'passwordTextField'"], :timeout => 20)
+  touch "* id:'passwordTextField'"
+  query "edittext id:'passwordTextField'", :setText => arg3
+  wait_for_elements_exist(["* id:'login_button'"], :timeout => 20)
+  touch "button id:'login_button'"
+  sleep(1)
+  wait_for_elements_exist(["* id:'pin'"], :timeout => 30)
+  wait_for_elements_exist(["* id:'confirm'"], :timeout => 30)
+  query "edittext id:'confirm'", :setText => arg4
+  query "edittext id:'pin'", :setText => arg4
+  wait_for_elements_exist(["* id:'set_pin_button'"], :timeout => 20)
+  touch "button id:'set_pin_button'"
+  wait_for_elements_exist(["* id:'action_bar_title'"], :timeout => 70)
+  sleep(1)
+end
+
+Then(/^I login to Sace client with org "(.*?)" email "(.*?)" password "(.*?)"$/) do |arg1,arg2,arg3|
+  wait_for_elements_exist(["* id:'org_name'"], :timeout => 20)
   query "edittext id:'org_name'", :setText => arg1
   wait_for_elements_exist(["* id:'login_new_org_button'"], :timeout => 20)
   touch "button id:'login_new_org_button'"
@@ -274,16 +306,10 @@ Then(/^I login to Sace with org "(.*?)" email "(.*?)" password "(.*?)" and PINs 
   query "edittext id:'passwordTextField'", :setText => arg3
   wait_for_elements_exist(["* id:'login_button'"], :timeout => 20)
   touch "button id:'login_button'"
-  sleep(5)
-  wait_for_elements_exist(["* id:'pin'"], :timeout => 30)
-  wait_for_elements_exist(["* id:'confirm'"], :timeout => 30)
-  query "edittext id:'confirm'", :setText => arg4
-  query "edittext id:'pin'", :setText => arg4
-  wait_for_elements_exist(["* id:'set_pin_button'"], :timeout => 20)
-  touch "button id:'set_pin_button'"
   wait_for_elements_exist(["* id:'action_bar_title'"], :timeout => 70)
-  sleep(1.5)
+  sleep(1)
 end
+
 
 
 Then(/^I navigate back from account to list of apps$/) do
@@ -324,6 +350,152 @@ Then(/^I reset current PIN "(.*?)" to "(.*?)" and confirmation "(.*?)"$/) do |ar
   performAction('enter_text_into_id_field', arg3, 'confirm_new_pin')
   touch "button id:'save'"
 end
+
+Then(/^I close organization list menu$/) do
+
+  wait_for_elements_exist(["* id:'action_bar_title'"], :timeout => 60)
+  sleep(0.1)
+  touch "textview id:'action_bar_title'"
+end
+
+Then(/^I navigate through history back$/) do
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 5)
+  touch "* id:'action_show_toolbar'"
+  wait_for_elements_exist(["* id:'go_back'"], :timeout => 120)
+  touch "ImageButton id:'go_back'"
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 5)
+  touch "* id:'action_show_toolbar'"
+end
+
+Then(/^I navigate through history forward$/) do
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 5)
+  touch "* id:'action_show_toolbar'"
+  wait_for_elements_exist(["* id:'go_forward'"], :timeout => 120)
+  touch "ImageButton id:'go_forward'"
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 5)
+  touch "* id:'action_show_toolbar'"
+end
+
+
+
+Then(/^I click on back button in entering org page$/) do
+  wait_for_elements_exist(["* id:'back_button'"], :timeout => 30)
+  touch "* id:'back_button'"
+end
+
+Then(/^I click on back button in entering email page$/) do
+  wait_for_elements_exist(["* id:'button'"], :timeout => 30)
+  touch "* id:'button'"
+end
+
+
+Then(/^I press back button on the top$/) do
+  wait_for_elements_exist(["* id:'home'"], :timeout => 30)
+  touch "* id:'home'"
+end
+
+
+Then(/^I refresh page$/) do
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 5)
+  touch "* id:'action_show_toolbar'"
+  wait_for_elements_exist(["* id:'reload'"], :timeout => 30)
+  touch "* id:'reload'"
+  wait_for_elements_exist(["* id:'action_show_toolbar'"], :timeout => 5)
+  touch "* id:'action_show_toolbar'"
+end
+
+Then(/^I cancel VPN auth form$/) do
+
+  wait_for_elements_exist(["* id:'vpn_auth_cancel'"], :timeout => 30)
+  touch "* id:'vpn_auth_cancel'"
+  sleep(2)
+  element_does_not_exist("* id:'vpn_auth_cancel'") 
+end
+
+
+Then(/^I try to find login element form from Act-On$/) do
+  sleep(1.8)
+  performAction('wait_for_no_progress_bars')
+  set_text "webView css:input[id=login]",  :setText => 'idk'
+end
+
+
+Then /^I close new tab$/ do
+  wait_for_elements_exist(["* id:'close_tab_button'"], :timeout => 30)
+  touch "* id:'close_tab_button'"
+end
+
+
+
+Then /^I wait until is page loaded$/ do
+  performAction('wait_for_no_progress_bars') 
+end
+
+Then /^I press the menu key$/ do
+  performAction('press_menu')
+end
+
+Then /^I check only one tab exist$/ do
+  element_does_not_exist("* id:'close_tab_button'")
+end
+
+
+
+
+Then /^I wait until page under VPN is loaded$/ do
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+  sleep(0.1)
+    performAction('wait_for_no_progress_bars')
+end
+
+
+
+Then /^I login to SAML OneLogin on Nexus5 on user dalsi@se.cz$/ do
+  sleep(10)
+  performAction("touch_coordinate", 240, 680)
+  system("adb shell input text dalsi@se.cz")
+  sleep(1)
+  performAction('send_key_enter')
+  sleep(1)
+  system("adb shell input text sasasasa1")
+  performAction('send_key_enter')
+  wait_for_elements_exist(["* id:'message'"], :timeout => 30)
+end
+
+
+Then /^I navigate back in SAML page$/ do
+  wait_for_elements_exist(["* id:'back_button'"], :timeout => 30)
+  sleep(2)
+  touch "* id:'back_button'"
+end
+
+Then /^I press upper tab$/ do
+  wait_for_elements_exist(["* id:'title'"], :timeout => 30)
+  touch "* id:'title'"
+end
+
+
+
+
+
+
+
+
 
 
 
