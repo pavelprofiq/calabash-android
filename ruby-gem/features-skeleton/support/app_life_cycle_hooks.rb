c31d36c8f445eb2f6516763e032258339a6cadb1
pavelprofiq <pavel.juchelka@profiq.cz>
2014-07-16 08:33:26 +0200

    Update app_life_cycle_hooks.rb

--- ruby-gem/features-skeleton/support/app_life_cycle_hooks.rb
+++ ruby-gem/features-skeleton/support/app_life_cycle_hooks.rb
@@ -2,6 +2,16 @@ require 'calabash-android/management/adb'
 require 'calabash-android/operations'
 
 Before do |scenario|
+  devices = connected_devices
+  if devices==["0336aa9a215fdbb4"]
+	$phone=["nexus5"]
+  elsif devices==["Here put device number for Nexus 5 device"]
+	$phone=["nexus5"]
+  elsif devices==["4790093530cd90b4"]
+	$phone=["s3mini"]
+  else 
+	$phone=["nexus5"]
+  end
   start_test_server_in_background
 end
 
@@ -12,3 +22,4 @@ After do |scenario|
   end
   shutdown_test_server
 end
+
